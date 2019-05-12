ebfcdfecf3d08dea8f129690d85d07af7096c654
dyboy2017 <1099718640@qq.com>
2019-05-12 12:31:46 +0800

    我也忘记了，更新了什么

--- function.php
+++ function.php
@@ -299,4 +299,25 @@ function admin_talk(){
         echo '<li><a href="/t">'.$row['content'].'</a></li>';
     }
 }
+
+/**
+* 判断当前用户是否为手机/移动端访问
+* :return Bool
+*/
+function is_mobile() {
+    if ( empty($_SERVER['HTTP_USER_AGENT']) ) {
+        $is_mobile = false;
+    } elseif ( strpos($_SERVER['HTTP_USER_AGENT'], 'Mobile') !== false
+        || strpos($_SERVER['HTTP_USER_AGENT'], 'Android') !== false
+        || strpos($_SERVER['HTTP_USER_AGENT'], 'Silk/') !== false
+        || strpos($_SERVER['HTTP_USER_AGENT'], 'Kindle') !== false
+        || strpos($_SERVER['HTTP_USER_AGENT'], 'BlackBerry') !== false
+        || strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== false
+        || strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mobi') !== false ) {
+            $is_mobile = true;
+    } else {
+        $is_mobile = false;
+    }
+    return $is_mobile;
+}
 ?>
